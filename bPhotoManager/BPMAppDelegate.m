10d402fe6d1868e1d9afb9e855efc30792ffbba4
畢 家寧 <bijianing@ggjd-2.local>
2012-07-28 12:28:39 +0900

    Initial Commit

---
+++ bPhotoManager/BPMAppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  BPMAppDelegate.m
+//  bPhotoManager
+//
+//  Created by 家寧 畢 on 12/07/28.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "BPMAppDelegate.h"
+
+@implementation BPMAppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
